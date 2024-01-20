a6686652d922321cdc4d6c857968ef48298ed350
andmnstr <andreyreseed@gmail.com>
2024-01-20 18:55:19 +0300

    fix: fix code according to prettier & eslint rules in '03-files-in-folder' task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,24 +2,25 @@ const fs = require('fs');
 const path = './03-files-in-folder/secret-folder/';
 const KBYTE_SIZE = 1024;
 
-fs.readdir(path, (err, files) => { 
-  if (err) 
-    console.log(err); 
-  else { 
-    files.forEach(file => {
+fs.readdir(path, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
       let fileName = '';
-      let extension = ''; 
+      let extension = '';
       let sizeKB;
 
-      const fileStat = fs.stat(path + file, (err, stats) => {
+      fs.stat(path + file, (err, stats) => {
         if (stats.isFile(file)) {
           fileName = file.slice(0, file.indexOf('.'));
+
           extension = file.slice(file.indexOf('.') + 1, file.length);
+
           sizeKB = (stats.size / KBYTE_SIZE).toFixed(3);
 
           console.log(`${fileName} - ${extension} - ${sizeKB}kb`);
         }
       });
-    }) 
-  } 
+    });
+  }
 });

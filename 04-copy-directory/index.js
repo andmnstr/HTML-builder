66d1ad5c2ea110e9da62d01e8b06e66e5a6e346f
andmnstr <andreyreseed@gmail.com>
2024-01-20 18:55:52 +0300

    fix: fix code according to prettier & eslint rules in '04-copy-directory' task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,57 +3,57 @@ const folder = './04-copy-directory/files/';
 const copyFolder = './04-copy-directory/files-copy/';
 
 function cleanUpDirectory(path) {
-    fs.readdir(path, (err, files) => {
-        if (err) {
-          console.log(err);
-        } else {
-            files.forEach(file => {
-              fs.unlink(`${path}${file}`, (err) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log(`File "${file}" was deleted from "${path}" folder`);
-                }
-              });
-            });
-        }
-    });
+  fs.readdir(path, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach((file) => {
+        fs.unlink(`${path}${file}`, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`File "${file}" was deleted from "${path}" folder`);
+          }
+        });
+      });
+    }
+  });
 }
 
 function createDirectory(path) {
-    fs.access(path, (err) => {
-        if (err) {
-          fs.mkdir(path, {recursive:false}, () => {
-            console.log('Copy of the folder has been created');
-          });
-        } else {
-          console.log('Requested directory is already exist');
-        }    
-    });
+  fs.access(path, (err) => {
+    if (err) {
+      fs.mkdir(path, { recursive: false }, () => {
+        console.log('Copy of the folder has been created');
+      });
+    } else {
+      console.log('Requested directory is already exist');
+    }
+  });
 }
 
 function copyFiles(dir1, dir2) {
-    fs.readdir(dir1, (err, files) => {
-        if (err) {
-            console.log(err);
-        } else {
-            files.forEach(file => {
-                fs.cp(`${dir1}${file}`, `${dir2}${file}`, (err) => {
-                    if (err) {
-                        console.log(`Error was thrown while copy "${file}" file`);
-                    } else {
-                        console.log(`File "${file}" was copied`);
-                    }
-                });
-            });
-        }
-    });
+  fs.readdir(dir1, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach((file) => {
+        fs.cp(`${dir1}${file}`, `${dir2}${file}`, (err) => {
+          if (err) {
+            console.log(`Error was thrown while copy "${file}" file`);
+          } else {
+            console.log(`File "${file}" was copied`);
+          }
+        });
+      });
+    }
+  });
 }
 
-function copyDir (dir1, dir2) {
-    createDirectory(dir2);
-    cleanUpDirectory(dir2);
-    copyFiles(dir1, dir2);
+function copyDir(dir1, dir2) {
+  createDirectory(dir2);
+  cleanUpDirectory(dir2);
+  copyFiles(dir1, dir2);
 }
 
-copyDir (folder, copyFolder);
+copyDir(folder, copyFolder);

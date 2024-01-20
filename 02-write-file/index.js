5123ff1c63883724b7944bbf06563d18130647c4
andmnstr <andreyreseed@gmail.com>
2024-01-20 18:53:11 +0300

    fix: fix code according to prettier & eslint rules in '02-write-file' task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,27 +4,30 @@ const rl = readline.createInterface(process.stdin, process.stdout);
 let inputText = '';
 
 fs.readFile('./02-write-file/text.txt', 'utf8', (error, data) => {
-    data === undefined ? inputText += '' : inputText += data;
-    console.log(inputText);
+  data === undefined ? (inputText += '') : (inputText += data);
+  console.log(inputText);
 });
 
-rl.setPrompt(`Hello, please, enter a text...      `);
+rl.setPrompt('Hello, please, enter a text...      ');
 rl.prompt();
 rl.on('line', (text) => {
-    inputText += `${text} `;
+  inputText += `${text} `;
 
-    if (text === 'exit') {
-        console.log('Goodbye, see you later!');
-        rl.close();
-    } else {
-        fs.writeFile('./02-write-file/text.txt', inputText, (error, data) => {
-            console.log('Your text has been writing');
-        });
-    }
+  if (text === 'exit') {
+    console.log('Goodbye, see you later!');
+    rl.close();
+  } else {
+    fs.writeFile('./02-write-file/text.txt', inputText, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Your text has been writing');
+      }
+    });
+  }
 });
 
 rl.on('SIGINT', () => {
-    console.log('Goodbye, see you later!');
-    rl.close();
+  console.log('Goodbye, see you later!');
+  rl.close();
 });
-

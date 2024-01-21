bc7775a459e3f0f6c8db4aabefba967b609cd31e
andmnstr <andreyreseed@gmail.com>
2024-01-21 22:53:04 +0300

    refactor: change 'fs.writeFile' method to 'new fs.WriteStream' in '02-write-file' task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,33 +1,44 @@
 const readline = require('readline');
 const fs = require('fs');
+const path = require('path');
+const filePath = path.join(`${__dirname}`, 'text.txt');
 const rl = readline.createInterface(process.stdin, process.stdout);
+const writeFile = new fs.WriteStream(filePath, { encoding: 'utf-8' });
 let inputText = '';
 
-fs.readFile('./02-write-file/text.txt', 'utf8', (error, data) => {
-  data === undefined ? (inputText += '') : (inputText += data);
-  console.log(inputText);
-});
+function writeFileContent(content) {
+  writeFile.on('open', () => {
+    rl.setPrompt('Hello! Please, enter a text.\n');
+    rl.prompt();
+    rl.on('line', (text) => {
+      content += text + ' ';
+      console.log(
+        'You can enter more text. For exit press "exit" or "CTRL + C"',
+      );
 
-rl.setPrompt('Hello, please, enter a text...      ');
-rl.prompt();
-rl.on('line', (text) => {
-  inputText += `${text} `;
-
-  if (text === 'exit') {
-    console.log('Goodbye, see you later!');
-    rl.close();
-  } else {
-    fs.writeFile('./02-write-file/text.txt', inputText, (err) => {
-      if (err) {
-        console.log(err);
+      if (text.toLowerCase() === 'exit') {
+        console.log('Goodbye, see you later!');
+        rl.close();
       } else {
-        console.log('Your text has been writing');
+        fs.writeFile(filePath, content, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('');
+          }
+        });
       }
     });
-  }
-});
 
-rl.on('SIGINT', () => {
-  console.log('Goodbye, see you later!');
-  rl.close();
-});
+    rl.on('SIGINT', () => {
+      console.log('Goodbye, see you later!');
+      rl.close();
+    });
+
+    rl.on('close', () => {
+      writeFile.close();
+    });
+  });
+}
+
+writeFileContent(inputText);

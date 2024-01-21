c89353ce3bcc816cc26691c4ce3179943b6969c5
andmnstr <andreyreseed@gmail.com>
2024-01-21 23:19:16 +0300

    refactor: add 'path' module  in '03-files-in-folder' task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
-const path = './03-files-in-folder/secret-folder/';
+const path = require('path');
+const folderPath = path.join(`${__dirname}`, 'secret-folder/');
 const KBYTE_SIZE = 1024;
 
-fs.readdir(path, (err, files) => {
+fs.readdir(folderPath, (err, files) => {
   if (err) console.log(err);
   else {
     files.forEach((file) => {
@@ -10,12 +11,11 @@ fs.readdir(path, (err, files) => {
       let extension = '';
       let sizeKB;
 
-      fs.stat(path + file, (err, stats) => {
+      fs.stat(folderPath + file, (err, stats) => {
         if (stats.isFile(file)) {
           fileName = file.slice(0, file.indexOf('.'));
-
-          extension = file.slice(file.indexOf('.') + 1, file.length);
-
+          extension = path.extname(file);
+          extension = extension.replaceAll('.', '');
           sizeKB = (stats.size / KBYTE_SIZE).toFixed(3);
 
           console.log(`${fileName} - ${extension} - ${sizeKB}kb`);
